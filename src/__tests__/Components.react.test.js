f2e70a7e152fbaae9b86e682eaff055a5d24f97a
Rayhan <nofussdufus@gmail.com>
2022-01-12 15:51:34 +0000

    Add Navbar test

--- src/__tests__/Components.react.test.js
+++ src/__tests__/Components.react.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+
+
 
 import Calculator from '../components/calculator/Calculator';
+
+import Navbar from '../components/navbar/Navbar';
+
 
 describe('Calculator', () => {
   const arr = [
@@ -41,5 +47,22 @@ describe('Calculator', () => {
     });
     expect(typeof buttons === 'object').toEqual(true);
     expect(buttons.length).toBe(19);
+  });
+});
+
+
+describe('Navbar', () => {
+
+  
+  test('should render the navbar and its children', async () => {
+    const {container} = render(<Navbar />, {wrapper: MemoryRouter});
+    const headerText = 'Math Magicians';
+    const navbar = container.querySelectorAll('li');
+    const navbarHeader = container.querySelector('h1');
+    expect(navbarHeader).toHaveTextContent(headerText);
+    expect(navbar.length).toBe(3);
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Calculator')).toBeInTheDocument();
+    expect(screen.getByText('Quote')).toBeInTheDocument();
   });
 });
