2471de8ea6ef40457e532229f8c169e5e840dac1
Anselem <anselemodims@gmail.com>
2022-01-12 15:13:30 +0100

    Test: wrote tests for click the click function on the buttons

--- src/__tests__/Components.react.test.js
+++ src/__tests__/Components.react.test.js
@@ -1,36 +1,45 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
 import Calculator from '../components/calculator/Calculator';
 
 describe('Calculator', () => {
-	test('should render calculator and its children', () => {
-		const arr = [
-			'AC',
-			'+/-',
-			'+',
-			'-',
-			'=',
-			'÷',
-			'x',
-			'%',
-			'1',
-			'2',
-			'3',
-			'4',
-			'5',
-			'6',
-			'7',
-			'8',
-			'9',
-			'0',
-      '.'
-		];
-		render(<Calculator />);
-		const buttons = screen.getAllByRole('button');
-		buttons.forEach((item) => {
-			expect(arr).toContain(item.name);
-      expect(item).not.toBeDisabled()
-		});
-	});
+  const arr = [
+    'AC',
+    '+/-',
+    '+',
+    '-',
+    '=',
+    '÷',
+    'x',
+    '%',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+    '.',
+  ];
+
+  render(<Calculator />);
+
+  test('should render calculator and its children', async () => {
+    const buttons = screen.getAllByRole('button');
+    await act(async () => {
+      buttons.forEach((item) => {
+        userEvent.click(item);
+        expect(item).not.toBeDisabled();
+        expect(arr).toContain(item.name);
+      });
+    });
+    expect(typeof buttons === 'object').toEqual(true);
+    expect(buttons.length).toBe(19);
+  });
 });
--- src/__tests__/logic/calculate.test.js
+++ src/__tests__/logic/calculate.test.js
@@ -1,85 +1,84 @@
-import calculate from "../../logic/calculate";
+import calculate from '../../logic/calculate';
 
 describe('Test calculate.js ability to add the number 1.1 to the number 2.2 and return a total of 3.3', () => {
-  var data = {
+  let data = {
     total: null,
     next: null,
-    operation: null
+    operation: null,
   };
 
   test('Check first button press of 1', () => {
     data = calculate(data, '1');
-    expect(data.next).toEqual("1");
+    expect(data.next).toEqual('1');
   });
 
   test('Check making first number a decimal', () => {
     data = calculate(data, '.');
-    expect(data.next).toEqual("1.");
+    expect(data.next).toEqual('1.');
   });
 
   test('Check increase of current button to 1.1', () => {
     data = calculate(data, '1');
-    expect(data.next).toEqual("1.1");
+    expect(data.next).toEqual('1.1');
   });
 
   test('Check adding an operator, +', () => {
     data = calculate(data, '+');
-    let result = {
+    const result = {
       total: '1.1',
       next: null,
-      operation: '+'
+      operation: '+',
     };
     expect(data).toEqual(result);
   });
 
   test('Check adding a second number', () => {
     data = calculate(data, '2');
-    let result = {
+    const result = {
       total: '1.1',
       next: '2',
-      operation: '+'
+      operation: '+',
     };
     expect(data).toEqual(result);
   });
 
   test('Check adding a dot to second number', () => {
     data = calculate(data, '.');
-    let result = {
+    const result = {
       total: '1.1',
       next: '2.',
-      operation: '+'
+      operation: '+',
     };
     expect(data).toEqual(result);
   });
 
   test('Check increasing the second number to 2.2', () => {
     data = calculate(data, '2');
-    let result = {
+    const result = {
       total: '1.1',
       next: '2.2',
-      operation: '+'
+      operation: '+',
     };
     expect(data).toEqual(result);
   });
 
   test('Check returning the sum of the two numbers, = button press', () => {
     data = calculate(data, '=');
-    let result = {
+    const result = {
       total: '3.3',
       next: null,
-      operation: null
+      operation: null,
     };
     expect(data).toEqual(result);
   });
 
   test('Check pressing the AC button, reset all variables to null expected', () => {
-    let result = {
+    const result = {
       total: null,
       next: null,
-      operation: null
+      operation: null,
     };
     data = calculate(data, 'AC');
     expect(data).toEqual(result);
   });
-  
 });
